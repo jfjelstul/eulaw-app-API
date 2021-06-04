9655ed9908878fa8a1081ba6a9b58c6e555c26a5
Josh Fjelstul <joshfjelstul@gmail.com>
2021-06-04 18:23:57 +0200

    Update eulaw-server.js

--- eulaw-server.js
+++ eulaw-server.js
@@ -35,32 +35,14 @@ v1.get("/", (req, res) => {
 });
 v1.get("/databases", (req, res) => {
   res.json([
-    { database_id: 1, database: "evoeu" },
-    { database_id: 2, database: "ecio" },
-    { database_id: 3, database: "euip" },
-    { database_id: 4, database: "eusa" },
-    { database_id: 5, database: "eutr" },
-    { database_id: 6, database: "eums" },
+    { database_id: 1, database: "evoeu", label: "EvoEU", name: "The Evolution of European Union Law (EvoEU) Database", description: "The Evolution of European Union Law (EvoEU) Database contains network data on the near-universe of legal documents in the acquis communautaire of the European Union (EU), including primary law, secondary law, and case law, and various types of links between documents (1951-2015). The database includes a nodes dataset and a links dataset." },
+    { database_id: 2, database: "ecio", label: "ECIO", name: "The European Commission Internal Organization (ECIO) Database", description: "The European Commission Internal Organization (ECIO) Database contains data on each commission of the European Commission, including data on commissioners, departments (Directorates-General and service departments), and commissioner portfolios (1958-2021). The database includes 11 datasets." },
+    { database_id: 3, database: "euip", label: "EUIP", name: "The European Union Infringement Procedure (EUIP) Database", description: "The European Union Infringement Procedure (EUIP) Database contains data on the European Commission infringement procedure, including data on infringement cases and decisions in infringement cases (2002-2020). The EUIP Database is part of the European Union Compliance Project (EUCP). The database includes 22 datasets, including time-series, cross-sectional time-series, directed dyad-year, and network data." },
+    { database_id: 4, database: "eusa", label: "EUSA", name: "The European Union State Aid (EUSA) Database", description: "The European Union State Aid (EUSA) Database contains data on state aid in the European Union (EU), including data on European Commission state aid cases (2000-2020), decisions in state aid cases (2000-2020), and state aid awards granted by member states (2016-2020). The EUSA Database is part of the European Union Compliance Project (EUCP). The database includes 27 datasets, including time-series, cross-sectional time-series, directed dyad-year, and network data." },
+    { database_id: 5, database: "eutr", label: "EUTR", name: "The European Union Technical Regulations (EUTR) Database", description: "The European Union Technical Regulations (EUTR) Database contains data on technical regulations proposed by European Union (EU) member states, including data on notifications of proposed technical regulations and comments and detailed opinions submitted by third-party member states and institutions under the 2015/1535 procedure (1986-2020). The EUTR Database is part of the European Union Compliance Project (EUCP). The database includes 15 datasets, including time-series, cross-sectional time-series, directed dyad-year, and network data." },
+    { database_id: 6, database: "eums", label: "EUMS", name: "The European Union Member State (EUMS) Database", description: "The European Union Member State (EUMS) Database contains general data on European Union (EU) member states, including data on accession, domestic political systems, membership in the European Economic and Monetary Union (EMU) and Schengen Area, legal obligations and opt-outs, and membership in other international organizations. The database includes 4 datasets." },
   ]);
 });
-// v1.get("/databases/EvoEU", (req, res) => {
-//   res.json({ message: "Welcome to the Evolution of European Union Law (EvoEU) Database module of the eulaw.app API!" });
-// });
-// v1.get("/databases/ECIO", (req, res) => {
-//   res.json({ message: "Welcome to the European Commission Internal Organization (ECIO) Database module of the eulaw.app API!" });
-// });
-// v1.get("/databases/EUIP", (req, res) => {
-//   res.json({ message: "Welcome to the European Union Infringement Procedure (EUIP) Database module of the eulaw.app API!" });
-// });
-// v1.get("/databases/EUSA", (req, res) => {
-//   res.json({ message: "Welcome to the European Union State Aid (EUSA) Database module of the eulaw.app API!" });
-// });
-// v1.get("/databases/EUTR", (req, res) => {
-//   res.json({ message: "Welcome to the European Union Technical Regulations (EUTR) Database module of the eulaw.app API!" });
-// });
-// v1.get("/databases/EUMS", (req, res) => {
-//   res.json({ message: "Welcome to the European Union Member States (EUMS) Database module of the eulaw.app API!" });
-// });
 
 // version 2 welcome message
 v2.get("/", (req, res) => {
